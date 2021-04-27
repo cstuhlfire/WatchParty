b8bd91365da2452c9766dc9bca24a80a468ab9e0
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-27 15:22:05 -0500

    Added relationships to models/index

---
+++ models/index copy.js
@@ -0,0 +1,52 @@
+const Users = require('./Users');
+const PartyLink = require('./PartyLink');
+const Parties = require('./Parties');
+const Shows = require('./Shows');
+const Genres = require('./Genres');
+
+// Shows have Genres
+Shows.belongsTo(Genres, {
+  foreignKey: 'show_id',
+});
+
+Parties.belongsTo(Shows, {
+  foriegnKey: "show_id",
+});
+
+Shows.hasMany(Parties, {
+  foriegnKey: "show_id",
+});
+
+// Usesrs have many Parties
+Users.hasMany(Parties, {
+  foreignKey: 'user_id',
+});
+
+// Parties have many Users
+Parties.hasMany(Users, {
+  foreignKey: 'party_id',
+});
+
+// Users belongToMany Parties (through PartyLink)
+Users.belongsToMany(Parties, {
+  through: {
+    model: PartyLink,
+    unique: false,
+  }
+});
+
+// Parties belongToMany Users (through PartyLink)
+Parties.belongsToMany(Users, {
+  through: {
+    model: PartyLink,
+    unique: false,
+  }
+});
+
+module.exports = { 
+  Users, 
+  Parties,
+  PartyLink,
+  Shows,
+  Genres
+ };
