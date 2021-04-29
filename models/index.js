0d7fb7fba99ecf16c9219cc32877bc0deb144d96
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-29 17:48:13 -0500

    Working on helper function to convert time

--- models/index.js
+++ models/index.js
@@ -3,36 +3,29 @@ const PartyLink = require('./PartyLink');
 const Parties = require('./Parties');
 const Genres = require('./Genres');
 
-// Showing have Genres
-// Showing.belongsTo(Genres, {
-//   foreignKey: 'show_id',
-// });
 
-// Parties.belongsTo(Showing, {
-//   foriegnKey: "show_id",
-// });
-
-// Showing.hasMany(Parties, {
-//   foriegnKey: "show_id",
-// });
-
-// // Usesrs have many Parties
 // Users.hasMany(Parties, {
 //   foreignKey: 'user_id',
 // });
 
-// Parties have many Users
 // Parties.hasMany(Users, {
 //   foreignKey: 'party_id',
 // });
 
-// Users belongToMany Parties (through PartyLink)
-// Users.belongsToMany(Parties, {
-//   through: {
-//     model: PartyLink,
-//     unique: false,
-//   }
-// });
+
+Users.belongsToMany(Parties, {
+  through: {
+    model: PartyLink,
+    unique: false,
+  }
+});
+
+Parties.belongsToMany(Users, {
+  through: {
+    model: PartyLink,
+    unique: false,
+  }
+});
 
 module.exports = {
   Users,
