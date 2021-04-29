9610272f9136398b0f0d4edfc1d07a978908b116
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-29 18:22:41 -0500

    Watchlist template and model index.js updates

--- models/index.js
+++ models/index.js
@@ -4,28 +4,27 @@ const Parties = require('./Parties');
 const Genres = require('./Genres');
 
 
-// Users.hasMany(Parties, {
-//   foreignKey: 'user_id',
-// });
-
-// Parties.hasMany(Users, {
-//   foreignKey: 'party_id',
-// });
-
-
-Users.belongsToMany(Parties, {
-  through: {
-    model: PartyLink,
-    unique: false,
-  }
+Parties.belongsTo(Users, {
+  foreignKey: 'host_id'
 });
 
-Parties.belongsToMany(Users, {
-  through: {
-    model: PartyLink,
-    unique: false,
-  }
+Users.hasMany(Parties, {
+  foreignKey: 'host_id',
 });
+
+// Users.belongsToMany(Parties, {
+//   through: {
+//     model: PartyLink,
+//     unique: false,
+//   }
+// });
+
+// Parties.belongsToMany(Users, {
+//   through: {
+//     model: PartyLink,
+//     unique: false,
+//   }
+// });
 
 module.exports = {
   Users,
