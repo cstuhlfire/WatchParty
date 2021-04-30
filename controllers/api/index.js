9cdeac774f24c60d85fddeaf1ba92db534594e2f
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-29 21:26:25 -0500

    Created elements to enter party details

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,10 @@ const usersRoutes = require('./usersRoutes');
 const genresRoutes = require('./genresRoutes');
 const partiesRoutes = require('./partiesRoutes');
 const partylinkRoutes = require('./partylinkRoutes');
-const showingRoutes = require('./showingRoutes');
 
 router.use('/profile', usersRoutes);
 router.use('/genres', genresRoutes);
-//router.use('/create', partiesRoutes);
+router.use('/create', partiesRoutes);
 router.use('/link', partylinkRoutes);
-router.use('/showing', showingRoutes);
-
 
 module.exports = router;
