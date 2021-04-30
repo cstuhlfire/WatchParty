c30d1a5cff5db745cffe0b1808d331c39bcd260b
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-29 19:08:04 -0500

    Updated /join route to grab user name

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,9 +6,9 @@ module.exports = {
   format_time: (date) => {
     // convert to 12 hour clock 
     if (date.getHours() >= 13){
-      date.getHours()-12 + ":"+date.getMinutes()+pm;
+      return date.getHours()-12 + ":"+date.getMinutes()+"pm";
     } else {
-      date.getHours()+":"+date.getMinutes()+am;
+      return date.getHours()+":"+date.getMinutes()+"am";
     }
     return time;
 
