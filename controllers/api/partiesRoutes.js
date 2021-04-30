798a2c1623d0a291660794bcad0b2293f6957a2e
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-30 03:39:16 -0500

    Add new party to database

--- controllers/api/partiesRoutes.js
+++ controllers/api/partiesRoutes.js
@@ -7,4 +7,19 @@ router.get('/', async (req, res) => {
   res.render('homepage');
   });
 
+
+router.post('/', async (req, res) => {
+  try {
+    const newParty = await Parties.create({
+      ...req.body,
+      //user_id: req.session.user_id,
+    });
+
+    res.direct('/');
+    //res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
